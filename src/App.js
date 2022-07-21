6bf30363ae965c957c6aa91d0e801df587aa5111
Kevin Diaz <kevinfd12@gmail.com>
2022-07-21 01:37:49 -0400

    Added BudgetCard Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Container from "react-bootstrap/Container"
+import { Button }from 'react-bootstrap';
+import { Stack } from "react-bootstrap"
+import { BudgetCard } from './components/BudgetCard';
 
 function App() {
-  return
+  return <Container className='my-4'>
+     <Stack direction='horizontal' gap='2' className='mb-4'>
+      <h1 className='me-auto'>Budgets</h1>
+      <Button variant="primary">Add Budget</Button>
+      <Button variant="outline-primary">Add Expense</Button>
+    </Stack>
+    <div style={{ display:"grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start",}}>
+    <BudgetCard name="Entertainment" gray amount={600} max={1000}></BudgetCard>
+    </div>
+  </Container>
 }
 
 export default App;
